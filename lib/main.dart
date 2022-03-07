4d24993398e57162115b1399944b3fb59479961f
Rabee-Omran <rabeeomran.dev@gmail.com>
2022-03-07 17:44:27 +0200

    4-  Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/posts_crud/presentation/bloc/post_bloc.dart';
+import 'features/posts_crud/presentation/pages/posts_page.dart';
+import 'injection_container.dart' as di;
+import 'injection_container.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Post CRUD',
-      home: Scaffold(
-        body: Container(
-          child: Text('Hello World'),
+    return BlocProvider(
+      create: (_) => sl<PostBloc>(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Post CRUD',
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(backgroundColor: Colors.blue.shade800),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.blue.shade800,
+          ),
+          buttonTheme: ButtonThemeData(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+          ),
         ),
+        home: PostsPage(),
       ),
     );
   }
